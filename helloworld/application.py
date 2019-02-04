3ed445451a0c19c9da33751f5473681b64da929e
epshteinv <47326538+epshteinv@users.noreply.github.com>
2019-02-04 22:10:23 +0200

    Update application.py

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,34 +1,80 @@
-#!flask/bin/python
-import json
-from flask import Flask, Response
-from helloworld.flaskrun import flaskrun
-import requests
+!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Simple Bot to reply to Telegram messages.
+
+This program is dedicated to the public domain under the CC0 license.
+
+This Bot uses the Updater class to handle the bot.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 
-application = Flask(__name__)
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
 
-bot_token = '665191062:AAGZuAnJGkIRQADm416VJ0tT'
 
-def get_url(method):
-  return "https://api.telegram.org/bot{}/{}".format(bot_token,method)
+def help(bot, update):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
 
-def process_message(update):
-    data = {}
-    data["chat_id"] = update["message"]["from"]["id"]
-    data["text"] = "I can hear you!"
-    r = requests.post(get_url("sendMessage"), data=data)
 
-@app.route("/{}".format(bot_token), methods=["POST"])
-def process_update():
-    if request.method == "POST":
-        update = request.get_json()
-        if "message" in update:
-            process_message(update)
-        return "ok!", 200
+def echo(bot, update):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    """Start the bot."""
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater("TOKEN")
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
 
 if __name__ == '__main__':
-    flaskrun(application)
-
-    
-    
-
+    main()
